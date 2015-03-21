a5255289cac2f4debc43b3279f6efd5e2149e671
Sambaran Roy <sambaran.roy@emids.com>
2015-03-21 07:16:35 +0530

    Implemented logic for Exacta Calculator. Added unit tests for Place calculator

--- test/dividendCalculator.test.js
+++ test/dividendCalculator.test.js
@@ -73,6 +73,29 @@ describe('Dividend Calculator', function() {
 
 	  beforeEach(function(){
 	  	PlaceCalculator = new Calculators.PlaceCalculator();
+	  	totalBets = [
+	  		{ 'product': 'P' , 'selection': '1' , 'stake': 10 },
+	  		{ 'product': 'P' , 'selection': '2' , 'stake': 10 },
+	  		{ 'product': 'P' , 'selection': '1' , 'stake': 20 },
+	  		{ 'product': 'P' , 'selection': '2' , 'stake': 20 },
+	  		{ 'product': 'P' , 'selection': '3' , 'stake': 10 },
+	  		{ 'product': 'P' , 'selection': '3' , 'stake': 20 },
+	  		{ 'product': 'P' , 'selection': '4' , 'stake': 30 },
+	  		{ 'product': 'P' , 'selection': '5' , 'stake': 30 } 				
+	  	];
+	  			
+	  	expectedResponseBets = {
+	  		'1': [ { 'product': 'P' , 'selection': '1' , 'stake': 10 },
+	  			{ 'product': 'P' , 'selection': '1' , 'stake': 20 }	],
+	  		'2': [ { 'product': 'P' , 'selection': '2' , 'stake': 10 },
+	  			{ 'product': 'P' , 'selection': '2' , 'stake': 20 }	],
+	  		'3': [ { 'product': 'P' , 'selection': '3' , 'stake': 10 },
+	  			{ 'product': 'P' , 'selection': '3' , 'stake': 20 }	]	  				  			
+	  	};
+	  	expectedResponseDividends = [
+	  		{	'1': 1.47 },
+	  		{ '2': 1.47 },
+	  		{ '3': 1.47 }];	  	
 	  });
 
 		it('Can be initialized properly', function() {
@@ -82,38 +105,92 @@ describe('Dividend Calculator', function() {
       assert.equal(PlaceCalculator.commisionPercentage,12);
 		});
 		it('Should take 12 percent commission before calculating dividends', function() {
+			var poolTotal = PlaceCalculator.CalculatePoolTotal(totalBets);
 
+			assert.equal(poolTotal, 132);
 		});
 
 		describe('Picks correct winning bets from the pool', function() {
+
+			beforeEach(function() {
+		  	betsResponseFirst = [ { 'product': 'P' , 'selection': '1' , 'stake': 10 },
+		  			{ 'product': 'P' , 'selection': '1' , 'stake': 20 }	];
+		  	betsResponseSecond = [ { 'product': 'P' , 'selection': '2' , 'stake': 10 },
+		  			{ 'product': 'P' , 'selection': '2' , 'stake': 20 }	];
+		  	betsResponseThird = [ { 'product': 'P' , 'selection': '3' , 'stake': 10 },
+		  			{ 'product': 'P' , 'selection': '3' , 'stake': 20 }	];
+			});
+
 			it('Should pick correct winning bets from the pool for first place', function() {
+				var winningBets = PlaceCalculator.CalculateWinningBets(totalBets, raceResults);
 
+				expect(winningBets).to.have.property('1');
+				expect(winningBets['1']).to.deep.equal(betsResponseFirst);
 			});
+
 			it('Should pick correct winning bets from the pool for second place', function() {
+				var winningBets = PlaceCalculator.CalculateWinningBets(totalBets, raceResults);
 
+				expect(winningBets).to.have.property('2');
+				expect(winningBets['2']).to.deep.equal(betsResponseSecond);
 			});
+
 			it('Should pick correct winning bets from the pool for third place', function() {
+				var winningBets = PlaceCalculator.CalculateWinningBets(totalBets, raceResults);
 
+				expect(winningBets).to.have.property('3');
+				expect(winningBets['3']).to.deep.equal(betsResponseThird);
 			});
-			it('Should return a single respone for winning bets with all three race results', function() {
 
+			it('Should return a single respone for winning bets with all three race results', function() {
+				var winningBets = PlaceCalculator.CalculateWinningBets(totalBets, raceResults);
+
+				expect(winningBets).to.deep.equal(expectedResponseBets);
 			});			
 		});
 
 		describe('Caclulates each dividend correctly', function() {
+
 			it('Should calculate the dividend for first place correctly', function() {
+				var winningBets = PlaceCalculator.CalculateWinningBets(totalBets, raceResults);
+				var dividends = PlaceCalculator.CalculateDividends(winningBets,totalBets);
 
+				expect(winningBets).to.have.property('1');
+				expect(dividends[0]).to.have.property('1');
+				expect(dividends[0]).to.have.deep.property('1', 1.47);
 			});
+
 			it('Should calculate the dividend for second place correctly', function() {
+				var winningBets = PlaceCalculator.CalculateWinningBets(totalBets, raceResults);
+				var dividends = PlaceCalculator.CalculateDividends(winningBets,totalBets);
 
+				expect(winningBets).to.have.property('2');
+				expect(dividends[1]).to.have.property('2');
+				expect(dividends[1]).to.have.deep.property('2', 1.47);
 			});
-			it('Should calculate the dividend for third place correctly', function() {
 
+			it('Should calculate the dividend for third place correctly', function() {
+				var winningBets = PlaceCalculator.CalculateWinningBets(totalBets, raceResults);
+				var dividends = PlaceCalculator.CalculateDividends(winningBets,totalBets);
+
+				expect(winningBets).to.have.property('3');
+				expect(dividends[2]).to.have.property('3');
+				expect(dividends[2]).to.have.deep.property('3', 1.47);
+				expect(winningBets).to.deep.equal(expectedResponseBets);
+				expect(dividends).to.deep.equal(expectedResponseDividends);
 			});
 		});
 
 		it('Should return three results', function() {
+				var winningBets = PlaceCalculator.CalculateWinningBets(totalBets, raceResults);
+				var dividends = PlaceCalculator.CalculateDividends(winningBets,totalBets);
 
+				assert.equal(dividends.length,3);
+				expect(dividends[0]).to.have.property('1');
+				expect(dividends[1]).to.have.property('2');
+				expect(dividends[2]).to.have.property('3');												
+				expect(winningBets).to.deep.equal(expectedResponseBets);
+				expect(dividends).to.deep.equal(expectedResponseDividends);
 		});
 	});
 
@@ -131,12 +208,12 @@ describe('Dividend Calculator', function() {
 	  		{ 'product': 'E' , 'selection': '1,3' , 'stake': 20 }	
 	  	];
 	  	expectedResponseBets = {
-	  		'1': [
-	  			{ 'product': 'W' , 'selection': '1,2' , 'stake': 10 },
-	  			{ 'product': 'W' , 'selection': '1,2' , 'stake': 20 }
+	  		'1,2': [
+	  			{ 'product': 'E' , 'selection': '1,2' , 'stake': 10 },
+	  			{ 'product': 'E' , 'selection': '1,2' , 'stake': 20 }
 	  		]
 	  	};
-	  	expectedResponseDividends = [{ '1,2': 2.83 }];	  	
+	  	expectedResponseDividends = [{ '1,2': 2.46 }];	  	
 	  });
 
 		it('Can be initialized properly', function() {
@@ -165,7 +242,7 @@ describe('Dividend Calculator', function() {
 
 			expect(winningBets).to.have.property('1,2');
 			expect(dividends[0]).to.have.property('1,2');
-			expect(dividends[0]).to.have.deep.property('1,2', 2.83);
+			expect(dividends[0]).to.have.deep.property('1,2', 2.46);
 			expect(winningBets).to.deep.equal(expectedResponseBets);
 			expect(dividends).to.deep.equal(expectedResponseDividends);	
 		});
@@ -176,7 +253,7 @@ describe('Dividend Calculator', function() {
 
 			assert.equal(dividends.length, 1);
 			assert.equal(propertyCount.length,1);
-			assert.equal(dividends[0][propertyCount[0]], 2.83);
+			assert.equal(dividends[0][propertyCount[0]], 2.46);
 			expect(winningBets).to.deep.equal(expectedResponseBets);
 			expect(dividends).to.deep.equal(expectedResponseDividends);
 		});
