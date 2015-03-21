a5faedca050ebf74f8c7e4e0892a4d91b8fcd4d6
Sambaran Roy <sambaran.roy@emids.com>
2015-03-21 05:55:12 +0530

    Added unit test for Tote Win Calculator

--- test/dividendCalculator.test.js
+++ test/dividendCalculator.test.js
@@ -5,9 +5,31 @@ describe('Dividend Calculator', function() {
 	describe('Win', function() {
 
 		var WinCalculator;
+		var totalBets;
+		var raceResults;
+		var expectedResponse;
 
 	  beforeEach(function(){
 	  	WinCalculator = new Calculators.WinCalculator();
+	  	raceResults = { 'first': '1', 'second': '2', 'third': '3' };
+	  	totalBets = [
+	  		{ 'product': 'W' , 'selection': '1' , 'stake': 10 },
+	  		{ 'product': 'W' , 'selection': '2' , 'stake': 10 },
+	  		{ 'product': 'W' , 'selection': '1' , 'stake': 20 },
+	  		{ 'product': 'W' , 'selection': '2' , 'stake': 20 },
+	  		{ 'product': 'W' , 'selection': '3' , 'stake': 40 },
+	  		{ 'product': 'P' , 'selection': '2' , 'stake': 10 },
+	  		{ 'product': 'P' , 'selection': '3' , 'stake': 10 },
+	  		{ 'product': 'E' , 'selection': '1,3' , 'stake': 10 },
+	  		{ 'product': 'E' , 'selection': '2,3' , 'stake': 10 }	  		 			  			  			  			  		
+	  	];
+	  	expectedResponseBets = {
+	  		'1': [
+	  			{ 'product': 'W' , 'selection': '1' , 'stake': 10 },
+	  			{ 'product': 'W' , 'selection': '1' , 'stake': 20 }
+	  		]
+	  	};
+	  	expectedResponseDividends = {	'1': 2.83 };
 	  });
 
 		it('Can be initialized properly', function() {
@@ -16,18 +38,37 @@ describe('Dividend Calculator', function() {
       assert.equal(WinCalculator.name,'Win');
       assert.equal(WinCalculator.commisionPercentage,15);
 		});
-		it('Should take 15 percent commission before calculating dividends', function() {
 
+		it('Should take 15 percent commission before calculating dividends', function() {
+			var poolTotal = WinCalculator.CalculatePoolTotal(totalBets);
+			
+			assert.equal(poolTotal, 85);
 		});
 		it('Should pick correct winning bets from the pool', function() {
+			var winningBets = WinCalculator.CalculateWinningBets(totalBets, raceResults);
 
+			assert.equal(winningBets, expectedResponseBets);
 		});
+
 		it('Should calculate dividends correctly', function() {
+			var winningBets = WinCalculator.CalculateWinningBets(totalBets, raceResults);
+			var dividends = WinCalculator.CalculateDividends(winningBets,totalBets);
 
+			assert.equal(winningBets, expectedResponseBets);
+			assert.equal(dividends, expectedResponseDividends);				
 		});
+
 		it('Should return a single result', function() {
+			var winningBets = WinCalculator.CalculateWinningBets(totalBets, raceResults);
+			var dividends = WinCalculator.CalculateDividends(winningBets,totalBets);
+			var propertyCount = Object.keys(dividends);
 
+			assert.equal(winningBets, expectedResponseBets);
+			assert.equal(dividends, expectedResponseDividends);
+			assert.equal(propertyCount.length,1);
+			assert.equal(dividends[propertyCount[0]],2.83);
 		});
+
 	});
 	describe('Place', function() {
 		var PlaceCalculator;
@@ -78,7 +119,7 @@ describe('Dividend Calculator', function() {
 		});
 	});
 	describe('Exacta', function() {
-		
+
 		var ExactaCalculator;
 
 	  beforeEach(function(){
